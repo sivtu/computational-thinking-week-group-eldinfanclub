f72a4471dda178ff682980393f7a717704dc0ffa
sivtu <geknxddelt@gmail.com>
2025-09-01 10:51:38 +0200

    fix: correct spelling errors in get_paragraph function

--- challenge_day1/jelle.py
+++ challenge_day1/jelle.py
@@ -5,9 +5,9 @@ def get_name() -> str:
 
 def get_paragraph(paragraph: int) -> str:
     if paragraph == 1: 
-        return "Mira had alwayz heard voices others could not. They whispered to her in dreams, and sometimes in the quiet moments of waking life. When the orb appeared, the voices grew louder, clearer."
+        return "Mira had always heard voices others could not. They whispered to her in dreams, and sometimes in the quiet moments of waking life. When the orb appeared, the voices grew louder, clearer."
     elif paragraph == 2: 
-        return "They told her to go to the forrest. They told her the others would be there. For the first time, the voices did not sound like a curse, but a guide."
+        return "They told her to go to the forest. They told her the others would be there. For the first time, the voices did not sound like a curse, but a guide."
     elif paragraph == 3: 
         return "When she enterd the clearing, the orb’s glow bent slightly toward her, as if recognizing her presence. She shivered but stood tall."
     else: 
