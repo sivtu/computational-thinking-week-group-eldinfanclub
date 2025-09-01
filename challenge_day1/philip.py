0144a58f0c19d0ccd0fdfc495a626f0264ce5773
Tom-Moriarty <tom.moriarty@student.uva.nl>
2025-09-01 10:37:08 +0200

    fix: correct typos in get_paragraph function

--- challenge_day1/philip.py
+++ challenge_day1/philip.py
@@ -5,9 +5,9 @@ def get_name() -> str:
 
 def get_paragraph(paragraph: int) -> str:
   if paragraph == 1: 
-    return "Lyra was the first to spot the strange glow rising from the forest’s edge. It flickered like a lantern, but pulsed too rhythmically to be man-made. She tightened her cloak, feeling a chill creep through the night ai."
+    return "Lyra was the first to spot the strange glow rising from the forest’s edge. It flickered like a lantern, but pulsed too rhythmically to be man-made. She tightened her cloak, feeling a chill creep through the night air."
   elif paragraph == 2: 
-    return "Curiosity always outweighed fear for Lyra. She stepped closer, listening to the whispers of leaves under her boots. Every sound felt amplified, like the forest itself was aware of her prese."
+    return "Curiosity always outweighed fear for Lyra. She stepped closer, listening to the whispers of leaves under her boots. Every sound felt amplified, like the forest itself was aware of her presence."
   elif paragraph == 3: 
     return "When she reached the clearing, she gasped. In the center stood a crystalline orb, floating above the ground. Its glow shifted colors like a heartbe."
   else: 
