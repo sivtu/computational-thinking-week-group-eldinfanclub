ac89f38f57bab7efd34081fbc4931a1a9fcb05e3
sivtu <geknxddelt@gmail.com>
2025-09-01 10:42:05 +0200

    fix: correct typos in get_paragraph function

--- challenge_day1/tom.py
+++ challenge_day1/tom.py
@@ -5,9 +5,9 @@ def get_name() -> str:
 
 def get_paragraph(paragraph: int) -> str:
   if paragraph == 1: 
-    return "Kael had been following Lyra at a distance. He never trusted sudden lights in the woods, his village’s stories alwyas ended with someone lost. Still, he couldn’t let her wander into danger alone."
+    return "Kael had been following Lyra at a distance. He never trusted sudden lights in the woods, his village’s stories always ended with someone lost. Still, he couldn’t let her wander into danger alone."
   elif paragraph == 2: 
-    return "He crouched behind a fallen tree, his bow ready. The orb’s glow reflected in his eyes, mesmerzing and unsettling at once. He wanted to shout, to call Lyra back, but the words froze in his throat."
+    return "He crouched behind a fallen tree, his bow ready. The orb’s glow reflected in his eyes, mesmerizing and unsettling at once. He wanted to shout, to call Lyra back, but the words froze in his throat."
   elif paragraph == 3: 
     return "Instead, he watched. If the orb made a move, Kael swore he’d strike, no matter how impossible the figt."
   else: 
