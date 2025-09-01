3b7748850dbc703b10a7a9749705a966cff4585e
sivtu <geknxddelt@gmail.com>
2025-09-01 10:03:31 +0200

    feat: include tom and nikola in team introduction

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,6 +1,8 @@
 import philip
 import justus
 import Twan
+import tom
+import nikola
 
 _TEAM_NAME: str = "Eldinfanclub"
 
@@ -9,6 +11,8 @@ def get_team() -> None:
   print(philip.get_name())
   print(justus.get_name())
   print(Twan.get_name())
+  print(tom.get_name())
+  print(nikola.get_name())
 
 
 get_team()
