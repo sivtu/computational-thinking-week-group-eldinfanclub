34f9bd12e10b66c53ac54e31b61671682ca0268b
JMB1191 <jellebosch2004@gmail.com>
2025-09-01 10:50:31 +0200

    fix: correct typos in get_paragraph function

--- challenge_day1/jelle.py
+++ challenge_day1/jelle.py
@@ -2,3 +2,14 @@ _NAME: str = "jelle"
 
 def get_name() -> str:
   return _NAME
+
+def get_paragraph(paragraph: int) -> str:
+    if paragraph == 1: 
+        return "Mira had alwayz heard voices others could not. They whispered to her in dreams, and sometimes in the quiet moments of waking life. When the orb appeared, the voices grew louder, clearer."
+    elif paragraph == 2: 
+        return "They told her to go to the forrest. They told her the others would be there. For the first time, the voices did not sound like a curse, but a guide."
+    elif paragraph == 3: 
+        return "When she enterd the clearing, the orb’s glow bent slightly toward her, as if recognizing her presence. She shivered but stood tall."
+    else: 
+        return "Paragraph not found."
+
