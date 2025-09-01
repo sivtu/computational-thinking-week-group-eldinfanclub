1ab9377af2e591688ca2559171dbbf87937a9539
Tom-Moriarty <tom.moriarty@student.uva.nl>
2025-09-01 10:43:58 +0200

    fix: correct typo in get_paragraph function

--- challenge_day1/justus.py
+++ challenge_day1/justus.py
@@ -9,6 +9,6 @@ def get_paragraph(paragraph: int) -> str:
   elif paragraph == 2: 
     return "He trudged through the undergrowth, his staff steadying him as he followed the strange light. For years, he had searched for purpose, for something to prove his wandering wasn’t wasted."
   elif paragraph == 3: 
-    return "When he saf Lyra and Kael in the clearing, he paus. He wasn’t the only one called here. Maybe the light had chosen more than just him."
+    return "When he saf Lyra and Kael in the clearing, he paused. He wasn’t the only one called here. Maybe the light had chosen more than just him."
   else: 
     return "Paragraph not found."
