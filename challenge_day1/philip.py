3191278e23db1b467835e48bab3c2ee7ffdc722b
Tom-Moriarty <tom.moriarty@student.uva.nl>
2025-09-01 10:28:16 +0200

    fix: correct wording in university description in get_paragraph function

--- challenge_day1/philip.py
+++ challenge_day1/philip.py
@@ -6,5 +6,5 @@ def get_name() -> str:
 def get_paragraph(paragraph: int) -> str:
   if paragraph == 1: return "The University of Amsterdam, founded in 1632, is one of Europe's largest and most prestigious research university."
   elif paragraph == 2: return "Located in the heart of Amsterdam, UvA offers over 200 English-taught programmes across diverse fields of study."
-  elif paragraph == 3: return "With its strong emphasis on research and innovation, UvA consistently ranks among the top universities world."
+  elif paragraph == 3: return "With its strong emphasis on research and innovation, UvA consistently ranks among the top universities worldwide."
   else: return "Paragraph not found."
