cc314a393acb37e3833b4d76e5176ca2f4207df2
Justus Tödtmann <justus.todtmann@student.uva.nl>
2025-09-01 09:47:02 +0200

    feat: include justus in team introduction functionality

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,10 +1,12 @@
 import philip
+import justus
 
 _TEAM_NAME: str = "Philip"
 
 def get_team() -> None:
   print(f"This is Team {_TEAM_NAME}. We are:")
   print(philip.get_name())
+  print(justus.get_name())
 
 
 get_team()
