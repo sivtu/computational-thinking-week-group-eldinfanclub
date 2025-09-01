e0b969c4bf5a3d1824d73771265a241418a8a32d
Justus Tödtmann <justus.todtmann@student.uva.nl>
2025-09-01 10:41:04 +0200

    fix: correct typos in get_paragraph function

--- challenge_day1/justus.py
+++ challenge_day1/justus.py
@@ -2,3 +2,13 @@ _NAME: str = "Justus"
 
 def get_name() -> str:
   return _NAME
+
+def get_paragraph(paragraph: int) -> str:
+  if paragraph == 1: 
+    return "Dorian had been wandering alone for days, a travelr without a home. When the glow rose into the sky, he thought it was a sign meant for him."
+  elif paragraph == 2: 
+    return "He trudged through the undergrowth, his staf steadying him as he followed the strange lig. For years, he had searched for purpose, for something to prove his wandering wasn’t wasted."
+  elif paragraph == 3: 
+    return "When he saf Lyra and Kael in the clearing, he paus. He wasn’t the only one called here. Maybe the light had chosen more than just him."
+  else: 
+    return "Paragraph not found."
