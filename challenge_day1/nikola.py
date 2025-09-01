319aaa12dc7b2486d1e48985eb0f861db3832dae
nk1309 <niki.kresojevic@gmail.com>
2025-09-01 10:54:26 +0200

    fix: add missing newline at end of file in nikola.py

--- challenge_day1/nikola.py
+++ challenge_day1/nikola.py
@@ -2,3 +2,13 @@ _NAME: str = "Nikola"
 
 def get_name() -> str:
   return _NAME
+
+def get_paragraph(paragraph: int) -> str:
+  if paragraph == 1: 
+    return "Seren wasn’t in the forest—yet she felt it. She was in the village square when the strange hum vibrated through her bones. Her pendant, a family heirloom, suddenly glowed faintly, as if echoing the orb miles away."
+  elif paragraph == 2: 
+    return "Startled, Seren dropped the bakstet she was carrying. Apples rolled across the cobblestones, but she barely noticed. The pull toward the woods was undeniable."
+  elif paragraph == 3: 
+    return "She left everything behind and ran, guided not by sight but by instinct. Whatever was happening, she was part of it now."
+  else: 
+    return "Paragraph not found."
