59eb58c8456bd86ecc371c6c522b26824b1f4691
nk1309 <niki.kresojevic@gmail.com>
2025-09-01 11:00:14 +0200

    fix: correct spelling error in get_paragraph function

--- challenge_day1/Twan.py
+++ challenge_day1/Twan.py
@@ -10,6 +10,6 @@ def get_paragraph(paragraph: int) -> str:
     elif paragraph == 2:
         return "He pushed through the trees with determination, sweat beeding on his brow. When he broke into the clearing, he froze at the sight of the orb and the gathering of familiar and unfamiliar faces."
     elif paragraph == 3:
-        return "Something inside him sturred—a realization that this night was the beginning of a story greater than any of them. The orb pulsed brighter, as if agreeing."
+        return "Something inside him stirred—a realization that this night was the beginning of a story greater than any of them. The orb pulsed brighter, as if agreeing."
     else:
         return "Paragraph not found."
