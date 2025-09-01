097e45baeafb9553d91dc702aa6e0aa59ed2ac76
Twanvdmaat <twanvandermaat@gmail.com>
2025-09-01 10:58:21 +0200

    Twan.py

--- challenge_day1/Twan.py
+++ challenge_day1/Twan.py
@@ -1,4 +1,15 @@
 _NAME: str = "Twan"
 
 def get_name() -> str:
+
   return _NAME
+
+def get_paragraph(paragraph: int) -> str:
+    if paragraph == 1:
+        return "Thalen was the last to arive. He had not been called by light or whispers but by loyalty—he refused to let his friends vanish without him."
+    elif paragraph == 2:
+        return "He pushed trough the trees with determination, sweat beeding on his brow. When he broke into the clearing, he froze at the sight of the orb and the gathering of familiar and unfamiliar faces."
+    elif paragraph == 3:
+        return "Something inside him sturred—a realization that this night was the beginning of a story greater than any of them. The orb pulsed brighter, as if agreeing."
+    else:
+        return "Paragraph not found."
