ba870d0a45c52d82352de4d8f3647ed5c8f48035
Justus Tödtmann <justus.todtmann@student.uva.nl>
2025-09-01 10:42:34 +0200

    fix: correct typo in get_paragraph function

--- challenge_day1/tom.py
+++ challenge_day1/tom.py
@@ -9,6 +9,6 @@ def get_paragraph(paragraph: int) -> str:
   elif paragraph == 2: 
     return "He crouched behind a fallen tree, his bow ready. The orb’s glow reflected in his eyes, mesmerizing and unsettling at once. He wanted to shout, to call Lyra back, but the words froze in his throat."
   elif paragraph == 3: 
-    return "Instead, he watched. If the orb made a move, Kael swore he’d strike, no matter how impossible the figt."
+    return "Instead, he watched. If the orb made a move, Kael swore he’d strike, no matter how impossible the fight."
   else: 
     return "Paragraph not found."
