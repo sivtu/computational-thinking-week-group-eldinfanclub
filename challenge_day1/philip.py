4f8a5748c1c0baa70da577d00c62b0516608dbbd
sivtu <geknxddelt@gmail.com>
2025-09-01 10:23:59 +0200

    feat: add get_paragraph function to provide paragraph

--- challenge_day1/philip.py
+++ challenge_day1/philip.py
@@ -2,3 +2,9 @@ _NAME: str = "Philip"
 
 def get_name() -> str:
   return _NAME
+
+def get_paragraph(paragraph: int) -> str:
+  if paragraph == 1: return "The University of Amsterdam, founded in 1632, is one of Europe's largest and most prestigious research universities."
+  elif paragraph == 2: return "Located in the heart of Amsterdam, UvA offers over 200 English-taught programmes across diverse fields of st."
+  elif paragraph == 3: return "With its strong emphasis on research and innovation, UvA consistently ranks among the top universities world."
+  else: return "Paragraph not found."
