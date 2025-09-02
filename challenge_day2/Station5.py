9685a987fc43810b4ad9ac2dd379b91726d37895
nk1309 <niki.kresojevic@gmail.com>
2025-09-02 14:12:19 +0200

    fix: update names in LT groups for consistency

--- challenge_day2/Station5.py
+++ challenge_day2/Station5.py
@@ -1,9 +1,9 @@
-LT1 = {"Daeho", "David", "Kaisa", "Oliver", "Sara", "Dan", "Ivar", "Lotte", "Riya", "Vassil", "Twan", "Ester", "Karolina", "Lena", "Margarita", "Anna", "Kien", "Klaudia", "Maliah", "Todd"}
-LT2 = {"Oumaima", "Mathilde", "Marie", "Anita", "Ziyan", "Bernardo", "Eleanor", "Lorijn", "Maria", "Younes", "Yvan", "Henning", "Liangyu", "Maciej", "Toprak", "Chris", "GengXin", "Mingze", "Phoebe"}
-LT3 = {"Betija", "Haider", "Kacper", "Sophie", "Amir", "Baltasar", "Isar", "Jelle", "Nicolas", "David", "Ipek", "Juan", "Marfa", "Maria", "Alissa", "Leopoldo", "Mies", "Jiaying", "Kaixin", "Mai", "Sem", "Tibbe"}
-LT4 = {"Justus", "Julia", "Philip", "Uli", "Vanessa", "Anna", "Ekaterina", "Thessa", "Tongfei", "Yang", "Benedikt", "Jan", "Nadee", "Osjah", "Tim", "Eliana", "Joana", "Peilin", "Pija", "Wenhao"}
-LT5 = {"Afua", "Cristina", "Greta", "Jace", "Laura", "Anna", "Bassant", "Ivan", "Juriaan", "Kiavash"}
-LT6 = {"Keitaro", "Nohemi", "Norina", "Yifan", "Yinan", "Luo", "Nikola", "Olesya", "Sophie", "Tom"}
+LT1 = {"Daeho", "David G", "Kaisa", "Oliver", "Sara", "Dan", "Ivar", "Lotte", "Riya", "Vassil", "Twan", "Ester", "Karolina", "Lena", "Margarita", "Anna P", "Kien", "Klaudia", "Maliah", "Todd"}
+LT2 = {"Oumaima", "Mathilde", "Marie", "Anita", "Ziyan", "Bernardo", "Eleanor", "Lorijn", "Maria P", "Younes", "Yvan", "Henning", "Liangyu", "Maciej", "Toprak", "Chris", "GengXin", "Mingze", "Phoebe"}
+LT3 = {"Betija", "Haider", "Kacper", "Sophie P", "Amir", "Baltasar", "Isar", "Jelle", "Nicolas", "David C", "Ipek", "Juan", "Marfa", "Maria L", "Alissa", "Leopoldo", "Mies", "Jiaying", "Kaixin", "Mai", "Sem", "Tibbe"}
+LT4 = {"Justus", "Julia", "Philip", "Uli", "Vanessa", "Anna A", "Ekaterina", "Thessa", "Tongfei", "Yang", "Benedikt", "Jan", "Nadee", "Osjah", "Tim", "Eliana", "Joana", "Peilin", "Pija", "Wenhao"}
+LT5 = {"Afua", "Cristina", "Greta", "Jace", "Laura", "Anna V", "Bassant", "Ivan", "Juriaan", "Kiavash"}
+LT6 = {"Keitaro", "Nohemi", "Norina", "Yifan", "Yinan", "Luo", "Nikola", "Olesya", "Sophie M", "Tom"}
 
 def solution_station_5(name: str) -> int:
   if name in LT1: return 1
@@ -13,3 +13,4 @@ def solution_station_5(name: str) -> int:
   if name in LT5: return 5
   if name in LT6: return 6
   raise ValueError(f"Name {name} not found in any LT group")
+
