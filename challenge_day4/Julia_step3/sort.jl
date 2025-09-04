fdf89a93284f2c87e3e1017b0e8e7bed1b2a2c95
Justus Tödtmann <justus.todtmann@student.uva.nl>
2025-09-04 11:26:56 +0200

    fix: update file paths and correct classification function in data processing script

--- challenge_day4/Julia_step3/sort.jl
+++ challenge_day4/Julia_step3/sort.jl
@@ -1,7 +1,9 @@
-using DataFrames, CSV, Statistics, DelimitedFiles
+using CSV
+using DataFrames
+using Statistics
 
 # Read the CSV file into a DataFrame
-people_df = CSV.File("data3.csv") |> DataFrame
+people_df = CSV.File("../fulldata/data3.csv") |> DataFrame
 
 # Function to classify a score based on quartiles
 function classify_score(score, quartiles)
@@ -32,12 +34,9 @@ for col_name in names(people_df)[2:end]
     quartiles = quantile(valid_data, [0.25, 0.5, 0.75])
 
     # Replace values with categories or 'low' if they are Float64
-    new_col = map(x -> x isa Float64 ? "low" : classification_score(x, quartiles), col_data)
+    new_col = map(x -> x isa Float64 ? "low" : classify_score(x, quartiles), col_data)
     people_df[!, col_name] = new_col
 end
 
 # Save the modified DataFrame back to a new CSV file
-CSV.write("data4.txt", people_df)
-
-# Save the modified DataFrame back to a new TXT file
-#writedlm("data4.txt", people_df, ',')
+CSV.write("../fulldata/data4.csv", people_df)
