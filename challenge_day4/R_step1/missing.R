3be75f9852720e897b41977dfa97d3e8bdbc6a18
Justus Tödtmann <justus.todtmann@student.uva.nl>
2025-09-04 09:59:47 +0200

    fix: update file paths for JSON data handling

--- challenge_day4/R_step1/missing.R
+++ challenge_day4/R_step1/missing.R
@@ -7,7 +7,7 @@ if (!requireNamespace("jsonlite", quietly = TRUE)) {
 library(jsonlite)
 
 # Read the JSON file into a data frame
-json_data <- fromJSON("data1.json")
+json_data <- fromJSON("../testdata/data1.json")
 
 # Convert the 'people' list to a data frame
 people_df <- as.data.frame(json_data$people)
@@ -24,10 +24,10 @@ for(col_name in names(people_df)) {
 }
 
 # Replace the 'people' list in the original data with the modified data frame
-json_data$people <- person_df
+json_data$people <- people_df
 
 # Convert the updated data back to JSON format
 json_text <- toJSON(json_data, pretty = TRUE)
 
 # Overwrite the original JSON file
-write(json_text, "data2.json")
+write(json_text, "../testdata/data2.json")
